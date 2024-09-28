fe49cd80d9ef3690d435d4e1195eff8e9afb9807
Rinaldi Sirait <23522052@std.stei.itb.ac.id>
2024-09-28 23:59:35 +0700

    add

--- app.py
+++ app.py
@@ -152,7 +152,7 @@ async def predict():
     return output
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8002)
 
 #Local test
 # uvicorn app:app --reload

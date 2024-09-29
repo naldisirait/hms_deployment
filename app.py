9d5ceac56150679c9e41a753d0ae4cb9de1609c7
Rinaldi Sirait <23522052@std.stei.itb.ac.id>
2024-09-29 17:42:16 +0700

    asd

--- app.py
+++ app.py
@@ -144,21 +144,29 @@ def do_prediction():
 
     # #5. Bundle the Output
     # #Convert output ml1 to dict
-    print(f"type of output_ml1 is {type(dates)}, panjang dates: {len(dates)}")
+    print(f"type of dates is {type(dates)}, panjang dates: {len(dates)}")
 
     dates, dict_output_ml1 = output_ml1_to_dict(dates=dates, output_ml1=output_ml1[0,:].tolist(), precipitation=ch_wilayah)
     dates, dict_output_hms = output_ml1_to_dict(dates=dates, output_ml1=debit_3days.tolist(), precipitation=ch_wilayah)
     for key,val in dict_output_ml1.items():
         print(key,val)
-        
+
     for key,val in dict_output_ml1.items():
         print(key,val)
     
     print(f"Panjang dates setelah dipotong pakai ml1: {len(dates)}")
     print("Berhasil bundle dict out 1")
+
     #Convert output ml2 to dict
-    dict_output_ml2_from_hms = output_ml2_to_dict(dates=dates,output_ml2=output_ml2_from_hms)
-    dict_output_ml2_from_ml1 = output_ml2_to_dict(dates=dates,output_ml2=output_ml2_from_ml1)
+    dict_output_ml2_from_hms = output_ml2_to_dict(dates=dates[-input_size_ml2:],output_ml2=output_ml2_from_hms)
+    dict_output_ml2_from_ml1 = output_ml2_to_dict(dates=dates[-input_size_ml2:],output_ml2=output_ml2_from_ml1)
+    
+    print("cek key, val dari output dict2 ml2 hms")
+    for key,val in dict_output_ml2_from_hms.items():
+        print(key,len(val))
+    print("cek key, val dari output dict2 ml2 ml2")
+    for key,val in dict_output_ml2_from_ml1.items():
+        print(key,len(val))
 
     end_run_pred = get_current_datetime()
     tend = time.time()

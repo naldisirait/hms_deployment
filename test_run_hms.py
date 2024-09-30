544510fc75d9476b49aecf4c670141e24d49e046
Rinaldi Sirait <23522052@std.stei.itb.ac.id>
2024-09-30 22:24:21 +0700

    update test run hms

--- test_run_hms.py
+++ test_run_hms.py
@@ -1,16 +1,25 @@
+import time
+import multiprocessing as mp
+print(f"CPUs avaible {mp.cpu_count()}")
+tstart = time.time()
 from RunHECHMSPalu import run_hms_palu
-from src.utils import inference_model, to_tensor, get_current_datetime, open_json_file
+from src.utils import open_json_file
 import numpy as np
-
 path_config_grid_to_df = "./configs/configuration of grided to df.json"
-
 conf_grid_to_df = open_json_file(path_config_grid_to_df)
 index_grided_chosen = conf_grid_to_df['indexes']
 
-dumm_prec = {}
-for index in index_grided_chosen:
-    dumm_prec[f"INDEX{index}"] = np.random.rand(720) + 2
+#create samples data to run
+samples_input_hms = []
+for i in range(10):
+    dumm_prec = {}
+    for index in index_grided_chosen:
+        dumm_prec[f"INDEX{index}"] = np.random.rand(720) + 2
+    samples_input_hms.append(dumm_prec)
 
 if __name__ == "__main__":
-    value_72, all_value = run_hms_palu(dumm_prec)
-    print(f"Return debit the last 3 days is {value_72}")
+    with mp.Pool(processes=5) as pool:
+        outputs = pool.map(run_hms_palu, samples_input_hms)
+    #value_72, all_value = run_hms_palu(dumm_prec)
+tend = time.time()
+print(f"Runtiime {tend-tstart}")
